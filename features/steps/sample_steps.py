becbc6688091e2d2e93dace41b427618a9a01995
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-11-12 13:53:39 +0000

    works, but needs functions moving to environment / rabbit_helper

--- features/steps/sample_steps.py
+++ features/steps/sample_steps.py
@@ -12,11 +12,7 @@ from config import Config
 
 @step("the sample file has been loaded fully into the action db")
 def loading_sample(context):
-    _clear_down_queues(Config.RABBITMQ_DELAYED_REDELIVERY_QUEUE)
-    _clear_down_queues(Config.RABBITMQ_SAMPLE_INBOUND_QUEUE)
-
-    # maybe clear this down again, sometimes seems to fall between the gaps..
-    _clear_down_queues(Config.RABBITMQ_DELAYED_REDELIVERY_QUEUE)
+    _clear_down_all_queues()
 
     sample_file_name = Path(Config.SAMPLE_FILE_PATH)
 
@@ -32,6 +28,24 @@ def loading_sample(context):
     time.sleep(10)
     _check_queue_is_empty(Config.RABBITMQ_SAMPLE_INBOUND_QUEUE)
     _check_queue_is_empty(Config.RABBITMQ_SAMPLE_TO_ACTION_QUEUE)
+
+
+def _clear_down_all_queues():
+    all_queues = _get_all_queues()
+
+    for q in all_queues:
+        # keep killing this Delayed queue, just to stop it redlivering anything in some mad race condition
+        _clear_down_queue(Config.RABBITMQ_DELAYED_REDELIVERY_QUEUE)
+        _clear_down_queue(q)
+
+
+def _get_all_queues():
+    uri = f'http://{Config.RABBITMQ_HOST}:{Config.RABBITMQ_MAN_PORT}/api/queues/%2f/'
+    response = requests.get(uri, auth=(Config.RABBITMQ_USER, Config.RABBITMQ_PASSWORD))
+    response.raise_for_status()
+    response_data = json.loads(response.content)
+
+    return [r['name'] for r in response_data]
 
 
 def _check_queue_is_empty(queue_name):
@@ -51,9 +65,7 @@ def _get_msg_count(queue_name):
     return response_data['messages']
 
 
-# GET all queues for api, and clear the fuckers down
-
-def _clear_down_queues(queue_name):
+def _clear_down_queue(queue_name):
     while True:
         uri = f'http://{Config.RABBITMQ_HOST}:{Config.RABBITMQ_MAN_PORT}/api/queues/%2f/{queue_name}/contents'
         response = requests.delete(uri, auth=(Config.RABBITMQ_USER, Config.RABBITMQ_PASSWORD))
