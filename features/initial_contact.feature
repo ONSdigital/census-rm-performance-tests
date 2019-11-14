81f3ee95442f1cf06a151c4d41239edd4987fc00
Adam <35268982+AdamHawtin@users.noreply.github.com>
2019-11-14 15:15:38 +0000

    Disable behave log/print capturing in concourse task (#8)
    * Disable behave log/print capturing in concourse task
    * Add print output for sample ingest time
    * Delete unused vars in concourse task

--- features/initial_contact.feature
+++ features/initial_contact.feature
@@ -1,8 +1,9 @@
 Feature: RM produces all initial contact print files within a time window
 
   Scenario: Sample load through to initial contact print file production
-    Given the sample file has been loaded fully into the action db
-    And all initial contact action rules have been scheduled for now
+    Given the sample file has been loaded
+    And the sample has been fully ingested into action scheduler database
+    When all initial contact action rules are scheduled for now
     Then all the initial contact print files are produced on the SFTP
     And they all have the correct line count
     And they are produced within the configured time limit
--- features/steps/sample_steps.py
+++ features/steps/sample_steps.py
@@ -9,11 +9,11 @@ from config import Config
 from features.environment import get_msg_count
 
 
-@step("the sample file has been loaded fully into the action db")
-def loading_sample(context):
+@step("the sample file has been loaded")
+def load_sample(context):
     sample_file_name = Path(Config.SAMPLE_FILE_PATH)
 
-    context.sample_load_time = datetime.utcnow()
+    context.sample_load_start_time = datetime.utcnow()
     load_sample_file(sample_file_name, context.action_plan_id,
                      context.action_plan_id,
                      host=Config.RABBITMQ_HOST, port=Config.RABBITMQ_PORT,
@@ -21,10 +21,16 @@ def loading_sample(context):
                      user=Config.RABBITMQ_USER, password=Config.RABBITMQ_PASSWORD,
                      queue_name=Config.RABBITMQ_SAMPLE_INBOUND_QUEUE)
 
+
+@step("the sample has been fully ingested into action scheduler database")
+def wait_for_full_sample_ingest(context):
     # Not ideal but seems to not work sometimes otherwise, shouldn't have time ramifications though
     time.sleep(10)
     _wait_for_queue_to_be_drained(Config.RABBITMQ_SAMPLE_INBOUND_QUEUE)
     _wait_for_queue_to_be_drained(Config.RABBITMQ_SAMPLE_TO_ACTION_QUEUE)
+    context.sample_fully_ingested_time = datetime.utcnow()
+    print(f'Time to fully ingest sample into action scheduler: '
+          f'[{str(context.sample_fully_ingested_time - context.sample_load_start_time)}]\n')
 
 
 def _wait_for_queue_to_be_drained(queue_name):
