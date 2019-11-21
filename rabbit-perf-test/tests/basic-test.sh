13484622e08fbf70398b48c9ebee380bda4d7a46
Richard Weeks <richard.weeks@qa.com>
2019-11-21 15:05:06 +0000

    Add RabbitMQ performance testing.

---
+++ rabbit-perf-test/tests/basic-test.sh
@@ -0,0 +1,1 @@
+bin/runjava com.rabbitmq.perf.PerfTest --uri amqp://guest:${RABBITMQ_PASSWORD}@rabbitmq-discovery.default.svc.cluster.local
---
+++ rabbit-perf-test/tests/publish-consume-spec.js
@@ -0,0 +1,2 @@
+[{'name': 'consume', 'type': 'simple', 'uri': 'amqp://guest:xHwiJyrI7l7LkwOsk9vDL7ph@rabbitmq-discovery.default.svc.cluster.local', 'params':
+[{'time-limit': 30, 'producer-count': 4, 'consumer-count': 2}]}]
