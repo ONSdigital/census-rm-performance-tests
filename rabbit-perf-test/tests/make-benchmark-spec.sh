525e6a02b274a36f9d15f752620cb088e60f0122
Richard Weeks <richard.weeks@qa.com>
2019-11-25 11:08:46 +0000

    Execute kubectl command.

--- rabbit-perf-test/tests/make-benchmark-spec.sh
+++ rabbit-perf-test/tests/make-benchmark-spec.sh
@@ -1,5 +1,5 @@
-USERNAME=kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-username}" | base64 --decode
-PASSWORD=kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-password}" | base64 --decode
+USERNAME=`kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-username}" | base64 --decode`
+PASSWORD=`kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-password}" | base64 --decode`
 cat  << EOF
 [{'name': 'consume', 'type': 'simple', 'uri': 'amqp://$USERNAME:$PASSWORD@rabbitmq-discovery.default.svc.cluster.local', 'params':
 [{'time-limit': 30, 'producer-count': 4, 'consumer-count': 2}]}]
