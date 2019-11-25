ed6ef09fe6e34225617ae1aa499f6f7b0cbbc51c
Richard Weeks <richard.weeks@qa.com>
2019-11-25 15:10:02 +0000

    Remove spec file.

--- rabbit-perf-test/tests/publish-consume-spec.js
+++
@@ -1,2 +0,0 @@
-[{'name': 'consume', 'type': 'simple', 'uri': 'amqp://guest:password@rabbitmq-discovery.default.svc.cluster.local', 'params':
-[{'time-limit': 30, 'producer-count': 4, 'consumer-count': 2}]}]
