06c9a0f833c43801e1691b5a2c15eaa4b0cab7a9
Richard Weeks <richard.weeks@qa.com>
2019-11-26 16:49:54 +0000

    Use env vars.

--- rabbit-perf-test/tests/make-benchmark-spec.sh
+++ rabbit-perf-test/tests/make-benchmark-spec.sh
@@ -1,6 +1,4 @@
-USERNAME=`kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-username}" | base64 --decode`
-PASSWORD=`kubectl get secret rabbitmq -o=jsonpath="{.data.rabbitmq-password}" | base64 --decode`
 cat  << EOF
-[{'name': 'consume', 'type': 'simple', 'uri': 'amqp://$USERNAME:$PASSWORD@rabbitmq-discovery.default.svc.cluster.local', 'params':
+[{'name': 'consume', 'type': 'simple', 'uri': 'amqp://$RABBITMQ_USER:$RABBITMQ_PASSWORD@rabbitmq-discovery.default.svc.cluster.local', 'params':
 [{'time-limit': 30, 'producer-count': 4, 'consumer-count': 2}]}]
 EOF
